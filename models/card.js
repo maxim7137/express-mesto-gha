6d90f1f204a483a85d40950737b36da95bc76f51
maxim7137 <maxim713@mail.ru>
2022-12-04 14:18:15 +0300

    feature: Подключился к Mongo

---
+++ models/card.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: {
+    type: Array,
+    default: [],
+  },
+});
+
+module.exports = mongoose.model('card', userSchema);
