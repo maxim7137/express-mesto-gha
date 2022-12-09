1836c854f1284d7018d602b3711e4c19e6be23b4
maxim713 <maxim713@mail.ru>
2022-12-09 21:20:48 +0300

    fix: ревью 1 limit и helmet

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const router = require('./routes');
 
 const { PORT = 3000, DB = 'mongodb://localhost:27017/mestodb' } = process.env;
+
 const app = express();
-const router = require('./routes');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use(limiter); // мидлвер для ограничения кол-во запросов. Для защиты от DoS-атак.
+app.use(helmet()); // мидлвер для для простановки security-заголовков, защ. от нек. уязвим.
 
 mongoose.connect(DB);
 
