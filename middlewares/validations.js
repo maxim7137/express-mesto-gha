0a461edf1bfc0119a17f02d4bdf7b3afd466a7fe
maxim713 <maxim713@mail.ru>
2022-12-17 20:42:44 +0300

    fix: cardCreate valid

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -165,11 +165,12 @@ module.exports.validateUserId = celebrate({
 module.exports.validateCardCreate = celebrate({
   body: Joi.object()
     .keys({
-      name: Joi.string().min(2).max(30).messages({
-        'string.min': 'Название от 2 до 30 символов, Joi',
-        'string.max': 'Название от 2 до 30 символов, Joi',
-        'any.required': 'Название карточки обязательно, Joi',
-      }),
+      name: Joi.string().required().min(2).max(30)
+        .messages({
+          'string.min': 'Название от 2 до 30 символов, Joi',
+          'string.max': 'Название от 2 до 30 символов, Joi',
+          'any.required': 'Название карточки обязательно, Joi',
+        }),
 
       link: Joi.string()
         .required()
