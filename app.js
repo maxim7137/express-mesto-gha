dc33d69f4bb8da6ea0567eb8e9fe26c977692fe4
maxim7137 <maxim713@mail.ru>
2022-12-19 23:12:01 +0300

    карточки и юзер.

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const cors = require('cors-express');
 const { errors } = require('celebrate');
 
 const router = require('./routes');
 const { errorHandler } = require('./middlewares/errorHandler');
 
-const { PORT = 3000, DB = 'mongodb://localhost:27017/mestodb' } = process.env;
+const { PORT = 3001, DB = 'mongodb://localhost:27017/mestodb' } = process.env;
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(DB);
 
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.use('/', require('./auth'));
 router.use('/cards', auth, require('./cards'));
 router.use('/users', auth, require('./users'));
 
-router.use('/*', auth, (req, res, next) => {
+router.use('/*', (req, res, next) => {
   next(new NotFoundError('Ресурс не найден. Проверьте URL и метод запроса'));
 });
 
